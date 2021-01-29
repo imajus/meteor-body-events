718097a2e97faf8fd6f7a5fdbcaf5023daae7f63
MickaelFM <faivrem@gmail.com>
2021-01-29 15:08:48 +0100

    upgrading to Meteor 2.0

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
   name: "gwendall:body-events",
   summary: "Get Template.body.events() working",
-  version: "0.1.7",
+  version: "0.2.0",
   git: "https://github.com/gwendall/meteor-body-events"
 });
 
 Package.on_use(function(api, where) {
-  api.use(["templating@1.0.7", "jquery@1.0.0"], "client");
+  api.use(["templating", "jquery"], "client");
   api.add_files(["lib.js"], "client");
 });

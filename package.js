0f4ac9707568d021754a6a17013f2083f0d21aa9
Denis Perov <denis.perov@gmail.com>
2021-02-10 13:44:20 +0300

    Support Meteor 1.4 dependency versions

--- package.js
+++ package.js
@@ -6,7 +6,7 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('2.0');
+  api.versionsFrom(['1.4', '2.0']);
   api.use(['blaze', 'templating', 'jquery'], "client");
   api.mainModule("lib.js", "client");
 });

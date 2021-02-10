86b6da029d49f25ec7d67252b427a549a075f3c5
Denis Perov <denis.perov@gmail.com>
2021-02-10 13:51:31 +0300

    Add ecmascript dependency

--- package.js
+++ package.js
@@ -1,12 +1,17 @@
 Package.describe({
   name: "imajus:body-events",
   summary: "Get Template.body.events() working in Meteor 2.0",
-  version: "0.0.1",
+  version: "0.0.2",
   git: "https://github.com/imajus/meteor-body-events"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom(['1.4', '2.0']);
-  api.use(['blaze', 'templating', 'jquery'], "client");
+  api.use([
+    'ecmascript',
+    'blaze',
+    'templating',
+    'jquery'
+  ], "client");
   api.mainModule("lib.js", "client");
 });
